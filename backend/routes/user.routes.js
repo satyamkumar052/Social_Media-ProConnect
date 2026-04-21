cb93a323ba07d9d76844801b9922c64b7da63786
Satyam Kumar <satyamkumar629301@gmail.com>
2026-04-21 12:15:35 +0530

    checked multer

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,8 @@ import multer from "multer";
 
 const router = Router();
 
+
+// files will be stored in uploads folder
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/')
