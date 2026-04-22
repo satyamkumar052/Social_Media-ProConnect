cb022c5cfa09fdecb6dbc7c6b3a5daf1100458f5
Satyam Kumar <satyamkumar629301@gmail.com>
2026-04-22 12:09:43 +0530

    format with proper spacing

--- frontend/src/Components/Navbar/index.jsx
+++ frontend/src/Components/Navbar/index.jsx
@@ -5,49 +5,49 @@ import { useDispatch, useSelector } from 'react-redux';
 import { reset } from '@/config/redux/reducer/authReducer';
 
 function Navbar() {
-    
+
     const router = useRouter();
 
     const dispatch = useDispatch();
 
     const authState = useSelector((state) => state.auth);
 
-  return (
-    <div className={styles.container}>
-       
-        <nav className={styles.navbar}>
+    return (
+        <div className={styles.container}>
 
-            <h1 onClick={() => {
+            <nav className={styles.navbar}>
+
+                <h1 onClick={() => {
                     router.push("/dashboard")
                 }}><img src="/images/proconnect.jpg" alt="logo" /></h1>
 
-            <div className={styles.navbarOptionContainer}>
+                <div className={styles.navbarOptionContainer}>
 
 
-            { authState.profileFetched  && <div>
-                <div style={{display:"flex", gap:"1.25rem"}}>
-                    <p onClick={() => {
-                        router.push("/profile");
-                    }} style={{fontWeight:"bold", cursor:"pointer"}}>Profile</p>
+                    {authState.profileFetched && <div>
+                        <div style={{ display: "flex", gap: "1.25rem" }}>
+                            <p onClick={() => {
+                                router.push("/profile");
+                            }} style={{ fontWeight: "bold", cursor: "pointer" }}>Profile</p>
 
-                    <p style={{cursor:"pointer"}} onClick={() => {
-                        localStorage.removeItem("token");
-                        dispatch(reset());
-                        router.push("/login");
-                    }}>Logout</p>
+                            <p style={{ cursor: "pointer" }} onClick={() => {
+                                localStorage.removeItem("token");
+                                dispatch(reset());
+                                router.push("/login");
+                            }}>Logout</p>
+
+                        </div>
+                    </div>}
+
+
+                    {!authState.profileFetched && <div className={styles.buttonJoin} onClick={() => router.push("/login")}>Be a part</div>}
 
                 </div>
-            </div> }
 
+            </nav>
 
-            { !authState.profileFetched  && <div className={styles.buttonJoin} onClick={() => router.push("/login")}>Be a part</div> }
-
-            </div>
-
-        </nav>
-
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Navbar
