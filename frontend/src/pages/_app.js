ab0f11ad0c003bb4eb433a5dc02158cd349ffd71
Satyam Kumar <satyamkumar629301@gmail.com>
2026-04-26 21:01:10 +0530

    simplify app provider

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -2,7 +2,6 @@ import { store } from "@/config/redux/store";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 
-
 export default function App({ Component, pageProps }) {
   return <>
 
